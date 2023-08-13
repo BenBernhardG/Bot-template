ebc276a09c4307609c50617d7182dfe128b513ea
BenBernhardG <138679020+BenBernhardG@users.noreply.github.com>
2023-08-13 14:51:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const fs = require('fs');
+require('dotenv').config();
+const { DISCORD_BOT_TOKEN } = process.env;
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 
+const client = new Client({ intents: GatewayIntentBits.Guilds });
+
+// Collections
+client.commands = new Collection();
+client.buttons = new Collection();
+client.menus = new Collection();
+client.modals = new Collection();
+
+client.login(DISCORD_BOT_TOKEN);
